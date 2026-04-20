25b8bda331789416e295798e697d81e4f3d4b112
Samuel1505 <oluwakoredeakinola@gmail.com>
2026-04-20 08:06:29 +0100

    added constant network and deploey

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+import { STACKS_MAINNET, STACKS_TESTNET } from '@stacks/network';
+
+export const FLYRA_DEPLOYER = 'SP1ZE73TWJ4WBFHZBBQJAMJDV23K678RXWPDGNHYF';
+
+export const NETWORKS = {
+  mainnet: STACKS_MAINNET,
+  testnet: STACKS_TESTNET,
+};
+
+export const DEFAULT_NETWORK = NETWORKS.mainnet;
+
+
